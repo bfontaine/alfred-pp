d33aa954e4a8acce0d1c9638bc6906afa61fea27
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-05 10:45:05 +0100

    URL opening implemented.

--- action.py
+++ action.py
@@ -1,9 +1,24 @@
 import alp
-from initdb import save_list
 import sys
+from initdb import save_list
+from subprocess import Popen
+
+def do_cmd(cmd):
+    pass # TODO
+
+def go_url(u):
+    Popen(['osascript', '-e', 'open location "%s"' % u])
 
 def main():
-    pass # TODO
+    if len(sys.argv) < 2:
+        return
+
+    typ, arg = sys.argv[1].split(':', 1)
+
+    if typ == 'cmd':
+        return do_cmd(arg)
+    elif typ == 'url':
+        return go_url(arg)
 
 if __name__ == '__main__':
     main()
